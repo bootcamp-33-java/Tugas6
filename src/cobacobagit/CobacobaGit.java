c20a23d3d446036821809a67c608c4da4400e187
aqira <aqira@users.noreply.github.com>
2020-01-20 14:00:21 +0700

    klklki

--- src/cobacobagit/CobacobaGit.java
+++ src/cobacobagit/CobacobaGit.java
@@ -29,6 +29,7 @@ public class CobacobaGit {
         System.out.println("asiap");
         System.out.println("asiap");
         System.out.println("asiap");
+        System.out.println("asial");
     }
     
 }
