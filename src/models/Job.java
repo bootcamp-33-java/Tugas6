b1ded359c5ba2605a189daca9fe5be9b313ce8f8
ASUS <ASUS@LAPTOP-IH4VF4HH>
2020-01-21 13:58:53 +0700

    Job

---
+++ src/models/Job.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Tutus
+ */
+public class Job {
+    private String id;
+    private String name;
+    private int minSalary;
+    private int maxSalary;
+    
+    public Job(){
+    }
+    
+    public Job(String id, String name, int minSalary, int maxSalary){
+        this.id = id;
+        this.name = name;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+}
